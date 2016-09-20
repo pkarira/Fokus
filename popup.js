4a0716ec40c268a60851ec4af2abcba4d1e3a3ef
pkarira <pulkit5208@gmail.com>
2016-09-21 01:01:34 +0530

    removed duplicate array variable

--- popup.js
+++ popup.js
@@ -1,18 +1,20 @@
 
 document.addEventListener('DOMContentLoaded', function() {
   var saveButton = document.getElementById('save');
-  var myArray=new Array();
+  //var array=new Array();
+  var siteUrl;
   saveButton.addEventListener('click', function()
    {
     chrome.storage.sync.get('myArray', function(data) { 
       alert(data.myArray.length);
-      myArray=data.myArray;
+      alert(data.myArray);
+     // array=data.myArray;
+     siteUrl = document.getElementById("siteurl").value;
+    (data.myArray).push(siteUrl);
+    chrome.storage.sync.set({'myArray':data.myArray}, function(){
     });
-  	var siteUrl = document.getElementById("siteurl").value;
-    myArray.push(siteUrl);
-  	chrome.storage.sync.set({'myArray':myArray}, function() {
-    alert(myArray);
-  	});
+    });
+  	
   },false);
   var viewButton = document.getElementById('view');
   viewButton.addEventListener('click', function() {
@@ -20,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
       var s="";
       for(var i=0;i<data.myArray.length;i++)
       {
-            s+=data.myArray[i];
+            s+=data.myArray[i]+"          ";
       }
       alert(s);
   	});
